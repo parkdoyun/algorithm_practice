7bf74a773516e00edfe26d0ae2b424ae163ba631
unknown <ehdqkdfjq97@gmail.com>
2022-11-14 21:33:35 +0900

    20221114

--- DFS/swea_4008.cpp
+++ DFS/swea_4008.cpp
@@ -4,63 +4,61 @@ using namespace std;
 
 // swea 4008 - 숫자 만들기
 // DFS, backtracking
-// backtracking으로 모든 연산 가짓수 계산
-// 마지막에 값 계산 후 최대, 최소 비교
+// 모든 경우의 수 다 하고 최대 최소 구하기
 
 int n;
-int num[13];
-int op[4]; // 0 :  + , 1 : -, 2 : *, 3 : /
-int max1, min1;
-int path[13];
+int path[13]; // 0 : +, 1 : -, 2 : *, 3 : /
+int arr[13];
+int operation[4]; // 연산자 갯수
+int max1 = -100000001;
+int min1 = 100000001;
 
 int cal()
 {
-	int res = num[0];
+	int res = arr[0];
 	for (int i = 0; i < n - 1; i++)
 	{
-		if (path[i] == 0) { // +
-			res = res + num[i + 1];
-		}
-		else if (path[i] == 1) { // -
-			res = res - num[i + 1];
-		}
-		else if (path[i] == 2) { // *
-			res = res * num[i + 1];
-		}
-		else // /
+		if (path[i] == 0)
 		{
-			res = res / num[i + 1];
+			res += arr[i + 1];
+		}
+		else if (path[i] == 1)
+		{
+			res -= arr[i + 1];
+		}
+		else if (path[i] == 2)
+		{
+			res *= arr[i + 1];
+		}
+		else // 3
+		{
+			res /= arr[i + 1];
 		}
 	}
-	
+
 	return res;
 }
 
 void DFS(int lv)
 {
-	if (lv == n - 1)
-	{
+	if (lv == n - 1) {
 		// 계산
-		int tmp = cal();
-		/*
-		for (int i = 0; i < n - 1; i++)
-		{
-			cout << path[i] << ' ';
-		}
-		cout << " => " << tmp << endl;
-		*/
-		if (tmp > max1) max1 = tmp;
-		if (tmp < min1) min1 = tmp;
+		int tmp_res = cal();
+		if (tmp_res > max1) max1 = tmp_res;
+		if (tmp_res < min1) min1 = tmp_res;
 		return;
 	}
 
 	for (int i = 0; i < 4; i++)
 	{
-		if (op[i] <= 0) continue;
-		op[i]--;
-		path[lv] = i;
-		DFS(lv + 1);
-		op[i]++;
+		if (operation[i] > 0)
+		{
+			operation[i]--;
+			path[lv] = i;
+			DFS(lv + 1);
+			path[lv] = -1;
+			operation[i]++;
+		}
 	}
 }
 
@@ -68,30 +66,38 @@ int main()
 {
 	int T;
 	cin >> T;
+	
 	for (int tc = 1; tc <= T; tc++)
 	{
+		cin >> n;
 		max1 = -100000001;
 		min1 = 100000001;
 
-		cin >> n;
 		for (int i = 0; i < 4; i++)
 		{
-			cin >> op[i];
+			cin >> operation[i];
 		}
+
 		for (int i = 0; i < n; i++)
 		{
-			cin >> num[i];
+			cin >> arr[i];
 		}
 
 		for (int i = 0; i < 4; i++)
 		{
-			if (op[i] <= 0) continue;
-			op[i]--;
-			path[0] = i;
-			DFS(1);
-			op[i]++;
+			if (operation[i] > 0)
+			{
+				operation[i]--;
+				path[0] = i;
+				DFS(1);
+				path[0] = -1;
+				operation[i]++;
+			}
 		}
+		
 		cout << '#' << tc << ' ' << max1 - min1 << '\n';
+
 	}
+
 	return 0;
 }
